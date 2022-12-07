3a3b6a4c92a88ba674e3f9c251266e8f83578889
Igor_U <fixtor75@gmail.com>
2022-12-07 14:35:21 +0300

    Задание 3 Готово

--- MinHeap.java
+++ MinHeap.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class MinHeap {
+public class MinHeap {
+
+
+	int[] arrayFinish = new int[6];
+//	int[] arr1 = {array[0]};
+//	int[] arr2 = {array[1], array[2]};
+//	int[] arr3 = {array[3], array[4], array[5]};
+
+
+	public int findMin(int[] arr) {
+
+		int min = arr[0];
+		for (int i = 0; i < arr.length; i++) {
+			if (min > arr[i]){
+				min = arr[i];
+			}
+		}
+		return min;
+	}
+
+	public static void main(String[] args) {
+		int[] array = new int[]{5, 8, 1, 4, 2, 0, 6, 2, 1};
+		MinHeap a = new MinHeap();
+		System.out.println(a.findMin(array));
+	}
 }
+
+
